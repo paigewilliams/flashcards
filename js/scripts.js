5395fe1768c1c547d726c9ac4e3e02a879ef56ef
Paige Williams <paw145@humboldt.edu>
2018-10-16 19:41:22 -0700

    changed h3 tags to p tags.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,21 @@
  $(document).ready(function() {
+   $(".well").click(function(){
+     $(this)children("p").slideToggle();
+     $(this).toggleClass("flashcard-clicked");
+   });
 
-  $(".click").click(function() {
-    $("#js-hide").slideToggle();
-    $("#js-show").slideToggle();
-  });
-
-  $(".click2").click(function() {
-    $("#op-hide").slideToggle();
-    $("#op-show").slideToggle();
-  });
-
-  $(".click3").click(function() {
-    $("#vr-hide").slideToggle();
-    $("#vr-show").slideToggle();
-  });
+  // $(".click").click(function() {
+  //   $("#js-hide").slideToggle();
+  //   $("#js-show").slideToggle();
+  // });
+  //
+  // $(".click2").click(function() {
+  //   $("#op-hide").slideToggle();
+  //   $("#op-show").slideToggle();
+  // });
+  //
+  // $(".click3").click(function() {
+  //   $("#vr-hide").slideToggle();
+  //   $("#vr-show").slideToggle();
+  // });
  });
